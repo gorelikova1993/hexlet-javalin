37a5847aafd730596b19bc8baf746fdc03849d51
Yulia <yulia.gorelikova1993@gmail.com>
2024-09-27 21:36:47 +0300

    lesson 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,10 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:6.1.3")
     implementation("gg.jte:jte:3.1.9")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:0.11.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,15 +1,44 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import org.example.hexlet.dto.courses.CoursePage;
+import org.example.hexlet.dto.courses.CoursesPage;
+import org.example.hexlet.model.Course;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class HelloWorld {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(HelloWorld.class);
+
+
     public static void main(String[] args) {
         //create app
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
+
         //описываем что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> ctx.render("index.jte"));
+
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.pathParamAsClass("id", Integer.class).get();
+            var course = new Course("Python", "Программирование");
+            var page = new CoursePage(course);
+            ctx.render("course/show.jte", model("page", page));
+        });
+        app.get("/courses", ctx -> {
+            var courses = List.of(new Course("Java", "Программирование"),
+                    new Course("JS", "Веб-разработка"));
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
+        });
+
         app.start(7070); //стартуем веб-сервер
     }
 }
